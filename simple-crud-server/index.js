cf7c2100aed0cdab69f723c96f34ae8aeaece7d6
RAKIBUL ISLAM <rakibul8226@gmail.com>
2024-04-23 09:43:34 +0600

    add new user info inot db server

--- simple-crud-server/index.js
+++ simple-crud-server/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
+//middleware
 app.use(cors());
 app.use(express.json())
 
@@ -24,6 +25,21 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    //inserting into db server
+    const database = client.db("userDB"); // db name
+    const userCollection = database.collection("users");
+
+    app.post('/users', async(req, res) =>{
+        const user = req.body;
+        console.log('new user', user);
+        
+        //inserting into db server
+        const result = await userCollection.insertOne(user);
+        res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
